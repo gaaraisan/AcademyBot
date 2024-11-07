7d1e3319c6c02eb05db8c2b2a156efd945207a3a
Ostap <Vampirisan@gmail.com>
2024-11-07 15:19:01 +0200

    Initial commit

--- main.py
+++ main.py
@@ -1,59 +1,56 @@
 from telegram import Update, InlineKeyboardButton, InlineKeyboardMarkup, InputFile
 from telegram.ext import Application, CommandHandler, CallbackQueryHandler, ContextTypes
 
-
-# Функція для відправки вітального повідомлення з фоновим зображенням
+# Функція для відправки вітального повідомлення з текстом
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
     welcome_text = (
-        "Вітаємо! Цей QR-код – не просто лінк, це комплімент від нас і запрошення до знайомства. "
-        "Переходь до чат-бота, щоб отримати відповіді на всі твої запитання та дізнатися більше про команду FAVBET. "
-        "Тут ми розкриємо все, що тебе цікавить!"
+        "Цей QR-код – це комплімент від нас і запрошення до нашої команди Spinner. "
+        "Тисни кнопку та дізнавайся, як стати частиною нашого світу!"
     )
 
-    keyboard = [[InlineKeyboardButton("Почати!", callback_data="main_text")]]
+    keyboard = [[InlineKeyboardButton("Далі!", callback_data="main_text")]]
     reply_markup = InlineKeyboardMarkup(keyboard)
 
-    # Відправка зображення як фонового
-    with open(r"C:\Users\Ostap\Desktop\TeleBotAcademy\channels4_profile.jpg", "rb") as photo:
-        await context.bot.send_photo(
-            chat_id=update.effective_chat.id,
-            photo=InputFile(photo),
-            caption=welcome_text,
-            reply_markup=reply_markup
-        )
+    # Відправка лише текстового повідомлення без зображення
+    await context.bot.send_message(
+        chat_id=update.effective_chat.id,
+        text=welcome_text,
+        reply_markup=reply_markup
+    )
 
-
-# Основний текст із кнопкою переходу до опису позицій
+# Основний текст із кнопкою переходу до наступного кроку
 async def main_text(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
     query = update.callback_query
     await query.answer()
-    await query.delete_message()  # Видалення попереднього повідомлення з зображенням
+    await query.delete_message()
 
     text = (
-        "Ми хочемо запросити тебе до команди FAVBET на одну з позицій – Game Presenter (ведучий/ведуча) "
-        "або Shuffler (шафлер) для проведення онлайн стрімів."
+        "Ми хочемо запросити тебе приєднатися до команди Spinner на одну з наступних позицій: \n\n"
+        "• Game Presenter (ведучий/ведуча) \n\n"
+        "• Shuffler (шафлер) \n\n"
+        "для проведення онлайн-стрімів."
     )
-    keyboard = [[InlineKeyboardButton("Цікавить", callback_data="benefits")]]
+    keyboard = [[InlineKeyboardButton("Цікаво", callback_data="main_tact")]]
     reply_markup = InlineKeyboardMarkup(keyboard)
 
     await context.bot.send_message(chat_id=update.effective_chat.id, text=text, reply_markup=reply_markup)
 
-
-# Опис переваг роботи
-async def benefits(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+# Додатковий текст із кнопкою переходу до опису переваг
+async def main_tact(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
     query = update.callback_query
     await query.answer()
+    await query.delete_message()
 
     text = (
-        "Уяви себе у ролі зірки нашої команди — у світі, де кожна твоя фраза додає грі шарму, а кожна дія створює атмосферу. "
+        "Уяви себе у ролі зірки нашої команди — у світі, де кожна твоя фраза додає етерн грі і шарму, а кожна дія створює атмосферу.  "
         "Ми шукаємо не просто колегу, а того, хто вміє бути на хвилі, відчувати момент і заряджати своєю енергією.\n\n"
-        "Майже на місці! Натисни на кнопку, щоб зробити перший крок до команди FAVBET."
+        "Spinner — новий, але крутий гравець на ринку, і ти можеш стати "
+        "частиною команди, про яку ще дізнаються. Ти готовий/-а?"
     )
-    keyboard = [[InlineKeyboardButton("Я готовий(-а)!", callback_data="final")]]
+    keyboard = [[InlineKeyboardButton("Готовий/(-а)", callback_data="final")]]
     reply_markup = InlineKeyboardMarkup(keyboard)
 
-    await query.edit_message_text(text=text, reply_markup=reply_markup)
-
+    await context.bot.send_message(chat_id=update.effective_chat.id, text=text, reply_markup=reply_markup)
 
 # Заключний текст із посиланням на рекрутера та зображенням студії
 async def final(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
@@ -62,16 +59,16 @@ async def final(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
 
     final_text = (
         "Твій інтерес важливий для нас! Просто надішли нашому рекрутеру Владі @vladyslava_hr будь-який емодзі 📨, "
-        "щоб дати нам знати, що ти готовий/-а розпочати цю подорож разом із FAVBET! Чекаємо на тебе в нашій команді!"
+        "щоб дати нам знати, що ти готовий/-а розпочати цю подорож разом із Spinner! "
+        "Чекаємо на тебе в нашій команді, де ти дізнаєшся все, що потрібно для яскравого старту!"
     )
 
-    with open(r"C:\Users\Ostap\Desktop\TeleBotAcademy\2024-11-04 14.55.24.jpg", "rb") as photo:
+    with open(r"C:\Users\Ostap\Desktop\TeleBotAcademy\front.png", "rb") as photo:
         await context.bot.send_photo(
             chat_id=update.effective_chat.id,
             photo=InputFile(photo),
             caption=final_text
         )
-
 
 # Основна функція для запуску бота
 def main() -> None:
@@ -82,12 +79,11 @@ def main() -> None:
 
     # Додаємо обробники для кнопок
     application.add_handler(CallbackQueryHandler(main_text, pattern="main_text"))
-    application.add_handler(CallbackQueryHandler(benefits, pattern="benefits"))
+    application.add_handler(CallbackQueryHandler(main_tact, pattern="main_tact"))
     application.add_handler(CallbackQueryHandler(final, pattern="final"))
 
     # Запускаємо бота
     application.run_polling()
-
 
 if __name__ == "__main__":
     main()
